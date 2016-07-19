ed8f04a0ce33addc18d8176b31e58881166f1f7d
Anastasiia <nastya.v.bondar@gmail.com>
2016-07-20 00:31:52 +0300

    Format js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,96 @@
-$(function(){
- var a=0;
- function add(){
- 	$("<li>").text(a).appendTo($(".list")).hide().slideDown().click(remove);
- 	a=a+1;
- }
+$(function() {
+    var a = 0;
 
-function remove(){
-	$(this).slideUp();
-}
+    function add() {
+        $("<li>").text(a).appendTo($(".list")).hide().slideDown().click(remove);
+        a = a + 1;
+    }
 
+    function remove() {
+        $(this).slideUp();
+    }
 
- function addClass(){
- 	$("ul.menu li").removeClass("active");
- 	$(this).addClass("active");
- }
+    function addClass() {
+        $("ul.menu li").removeClass("active");
+        $(this).addClass("active");
+    }
 
- function move(e){
- 	var dx=e.clientX;
- 	var dy=e.clientY;
- 	console.log("hello");
- 	console.log(e, dx,dy);
- 	$(".add-button").css("top", dy).css("left", dx);
- }
+    function move(e) {
+        var dx = e.clientX;
+        var dy = e.clientY;
+        console.log("hello");
+        console.log(e, dx, dy);
+        $(".add-button").css("top", dy).css("left", dx);
+    }
 
- function checkAge(){
- 	var age= $("input[name='age']").val();
- 	if (age>=18){
- 		$(".message").fadeOut();
- 	}else{
- 		$(".message").fadeIn();
- 	}
-   	console.log(age);
- } 
-function checkAvto(){
-	var exp=$("input[name='expirience']").val();
-	if(exp<=3){
-		$(".exp-info").fadeIn();
-	}else{
-		$(".exp-info").fadeOut();
-	}
-	console.log(exp);
-}
+    function checkAge() {
+        var age = $("input[name='age']").val();
+        if (age >= 18) {
+            $(".message").fadeOut();
+        } else {
+            $(".message").fadeIn();
+        }
+        console.log(age);
+    }
 
-function sayHello(){
-	var date = new Date();
-	var timeString = date.toLocaleTimeString();
-	var message = $("<p>").addClass("message").text("Hello");
-	var time = $("<p>").addClass("time").text(timeString);
-	$("<li>").append(message).append(time).prependTo($(".messages")).hide().slideDown();
-}
+    function checkAvto() {
+        var exp = $("input[name='expirience']").val();
+        if (exp <= 3) {
+            $(".exp-info").fadeIn();
+        } else {
+            $(".exp-info").fadeOut();
+        }
+        console.log(exp);
+    }
 
-function convert(){
-	var number = $("input.dec").val();
-	number = parseInt(number,10);
-	var hex = number.toString(16);
-	$("input.hex").val(hex);
-	var bin = number.toString(2);
-	$("input.bin").val(bin);
-	var oct = number.toString(8);
-	$("input.oct").val(oct);
-	console.log("Hello",number,hex);
-}
+    function sayHello() {
+        var date = new Date();
+        var timeString = date.toLocaleTimeString();
+        var message = $("<p>").addClass("message").text("Hello");
+        var time = $("<p>").addClass("time").text(timeString);
+        $("<li>").append(message).append(time).prependTo($(".messages")).hide().slideDown();
+    }
 
-function fillRandom(){
-	var random = parseInt(Math.random()*200);
-	$("input.dec").val(random);
-	convert();
-	console.log(random);
-}
+    function convert() {
+        var number = $("input.dec").val();
+        number = parseInt(number, 10);
+        var hex = number.toString(16);
+        $("input.hex").val(hex);
+        var bin = number.toString(2);
+        $("input.bin").val(bin);
+        var oct = number.toString(8);
+        $("input.oct").val(oct);
+        console.log("Hello", number, hex);
+    }
 
-function colorBody(){
-   var r= parseInt(Math.random()*16).toString(16);
-   var g= parseInt(Math.random()*16).toString(16);
-   var b= parseInt(Math.random()*16).toString(16);
-   var rgb =("#" + r + g +b);
-   $("body").css("background-color", rgb)
-   console.log(r,g,b,rgb);
-}
+    function fillRandom() {
+        var random = parseInt(Math.random() * 200);
+        $("input.dec").val(random);
+        convert();
+        console.log(random);
+    }
 
- 	// $("body").click(move);
+    function colorBody() {
+        var r = parseInt(Math.random() * 16).toString(16);
+        var g = parseInt(Math.random() * 16).toString(16);
+        var b = parseInt(Math.random() * 16).toString(16);
+        var rgb = ("#" + r + g + b);
+        $("body").css("background-color", rgb)
+        console.log(r, g, b, rgb);
+    }
 
-	$(".add-button").css("color", "green").click(add);
+    $(".add-button").css("color", "green").click(add);
 
- 	$("ul.menu li").click(addClass);
+    $("ul.menu li").click(addClass);
 
- 	$(".send-info").click(checkAge);
+    $(".send-info").click(checkAge);
 
- 	$(".send-info").click(checkAvto);
+    $(".send-info").click(checkAvto);
 
- 	$(".button-hello").click(sayHello);																		
+    $(".button-hello").click(sayHello);
 
     $(".dec").keyup(convert);
- 	$(".rnd").click(fillRandom);
- 	fillRandom();
- 	$(".color-page").click(colorBody);
+    $(".rnd").click(fillRandom);
+    fillRandom();
+    $(".color-page").click(colorBody);
 })
